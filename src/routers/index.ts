d604e484810a4cbeba9dd240bab252059fcac358
monique <moniquejp16@gmail.com>
2023-12-15 02:34:16 -0300

    feat: creation of routes

---
+++ src/routers/index.ts
@@ -0,0 +1,2 @@
+export * from "./routerProducts";
+export * from "./routerRequests";
---
+++ src/routers/routerRequests.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const RequestRouter = Router();
+
+RequestRouter.post('/request', controllerRequest)
+
+export { RequestRouter };
