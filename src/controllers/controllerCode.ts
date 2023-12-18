a6a8c7a781ace5b56b8e69d31a90ca28e9ffb812
monique <moniquejp16@gmail.com>
2023-12-18 20:52:13 -0300

    feat: getting the code

---
+++ src/controllers/controllerCode.ts
@@ -0,0 +1,9 @@
+import serviceCode from "@/services/serviceCode";
+import { Request, Response } from "express";
+import httpStatus from "http-status";
+
+
+export async function getCode(req: Request, res: Response) {
+    const code = await serviceCode.getProducts();
+    return res.status(httpStatus.OK).send(code);
+}
--- src/controllers/controllerProduct.ts
+++ src/controllers/controllerProduct.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 import httpStatus from "http-status";
 
 
-export async function getProducts(req: Request, res: Response){
-const allProducts= await serviceProducts.getProducts();
- return res.status(httpStatus.OK).send(allProducts);
+export async function getProducts(req: Request, res: Response) {
+    const allProducts = await serviceProducts.getProducts();
+    return res.status(httpStatus.OK).send(allProducts);
 }
