0e973610e4c3c0d33544079b51ca32d6967fa062
monique <moniquejp16@gmail.com>
2023-12-19 03:22:20 -0300

    feat: correct parameter typing

--- src/services/serviceRequest.ts
+++ src/services/serviceRequest.ts
@@ -1,10 +1,12 @@
 import { RequestSchemaTotalType } from "@/controllers/controlllerRequest";
-import repositoryRequest from "@/repositories/repositoryRequest";
+import { repositoryRequest } from "@/repositories/repositoryRequest";
 
-async function postRequest(choice: RequestSchemaTotalType) {
+
+async function postRequest(choice: RequestSchemaTotalType[]) {
     const result = await repositoryRequest.postRequest(choice);
     return result;
 };
+
 const serviceRequest = {
     postRequest
 };
