ff5475e1b8766a956d8e42f004a3b7d7cf39f71d
monique <moniquejp16@gmail.com>
2023-12-19 04:47:32 -0300

    feat: update code

--- src/controllers/controllerCode.ts
+++ src/controllers/controllerCode.ts
@@ -3,8 +3,21 @@ import serviceCode from "@/services/serviceCode";
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 
-
 export async function getCode(req: Request, res: Response) {
     const code = await serviceCode.getCode();
     return res.status(httpStatus.OK).send(code);
 }
+
+export async function updateCode(req: Request, res: Response) {
+    const updatedCode = req.body;
+    const newCode = (parseInt(updatedCode)) - 1
+    console.log(updatedCode)
+    console.log(newCode)
+    const result = await serviceCode.updateCode(updatedCode, newCode);
+
+    return res.status(httpStatus.CREATED).send(result);
+}
+
+export type Code = {
+    id: number;
+};
