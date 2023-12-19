b0f4a057131f9491a7b695b3fbe72558004fa4b9
monique <moniquejp16@gmail.com>
2023-12-19 02:50:58 -0300

    fix: import serviceRequest

--- src/controllers/controlllerRequest.ts
+++ src/controllers/controlllerRequest.ts
@@ -1,33 +1,33 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
-import serviceRequest from "@/services/serviceRequest";
+import serviceRequest from "../services/serviceRequest";
 
 export async function postRequest(req: Request, res: Response) {
-  const  choice  = req.body as RequestSchemaTotalType;
+    const choice = req.body as RequestSchemaTotalType;
 
-  const result = await serviceRequest.postRequest(choice);
+    const result = await serviceRequest.postRequest(choice);
 
-  return res.status(httpStatus.CREATED).send(result);
+    return res.status(httpStatus.CREATED).send(result);
 }
 
 type FollowUpType = {
     id: number;
     item: string;
     price: string;
-  };
-  
-  type RequestSchemaType = {
+};
+
+type RequestSchemaType = {
     id: number;
     image: string;
     name: string;
     price: number;
     description: string;
-  };
-  
-  type RequestSchemaTotalType = {
+};
+
+type RequestSchemaTotalType = {
     ProductSpecific: RequestSchemaType;
     counter: number;
     followUp?: FollowUpType;
     observationText: string;
     total: string;
-  };
+};
