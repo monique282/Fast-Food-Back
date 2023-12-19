ba9e730a29ed797e28d9f6014d0cbcdfa03db462
monique <moniquejp16@gmail.com>
2023-12-19 08:23:21 -0300

    feat: putting joi validation

--- src/routers/routerReady.ts
+++ src/routers/routerReady.ts
@@ -1,11 +1,12 @@
 
 import { postReady } from '@/controllers/controllerReady';
 import { validateJoiForAll } from '@/middlewares/middlewaresValidate';
+import { ReadySchema } from '@/schemas/schemasRequest';
 import { Router } from 'express';
 
 const RequestReady = Router();
 
-RequestReady.post('/updateReady', validateJoiForAll(), postReady)
+RequestReady.post('/updateReady', validateJoiForAll(ReadySchema), postReady)
 
 export { RequestReady };
 
