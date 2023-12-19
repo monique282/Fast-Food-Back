4e27ba7079692ff785139c0ec64034cb2b5d168c
monique <moniquejp16@gmail.com>
2023-12-19 02:45:03 -0300

    feat: importing from controllers

--- src/routers/routerRequests.ts
+++ src/routers/routerRequests.ts
@@ -1,10 +1,11 @@
+import { postRequest } from '@/controllers/controlllerRequest';
 import { validateJoiForAll } from '@/middlewares/middlewaresValidate';
 import { RequestSchemaTotal } from '@/schemas/schemasRequest';
 import { Router } from 'express';
 
 const RequestRouter = Router();
 
-RequestRouter.post('/request', validateJoiForAll(RequestSchemaTotal),)
+RequestRouter.post('/request', validateJoiForAll(RequestSchemaTotal), postRequest)
 
 export { RequestRouter };
 
