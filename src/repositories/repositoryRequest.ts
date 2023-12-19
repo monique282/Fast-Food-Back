c969b0cbfd901a1cbc8ebdd49fc23f11e353c1ac
monique <moniquejp16@gmail.com>
2023-12-19 07:32:39 -0300

    feat: putting id

--- src/repositories/repositoryRequest.ts
+++ src/repositories/repositoryRequest.ts
@@ -2,27 +2,28 @@ import prisma from '@/config/database';
 import { FollowUpType, RequestSchemaTotalType } from '@/protocols';
 
 async function postRequest(data: RequestSchemaTotalType[]): Promise<void> {
-    for (const requestData of data) {
-        const { ProductSpecific, counter, followUp, observationText, total, nameClient, code } = requestData;
-        const { id, image, name, price, description } = ProductSpecific;
+  for (const requestData of data) {
+    const { ProductSpecific, counter, followUp, observationText, total, nameClient, code } = requestData;
+    const { id, image, name, price, description } = ProductSpecific;
 
-        await prisma.request.create({
-            data: {
-                image,
-                name,
-                price,
-                description,
-                counter,
-                observationText,
-                total,
-                nameClient,
-                code,
-                followUps: {
-                  create: followUp,
-                },
-              },
-            });
-    }
+    await prisma.request.create({
+      data: {
+        id,
+        image,
+        name,
+        price,
+        description,
+        counter,
+        observationText,
+        total,
+        nameClient,
+        code,
+        followUps: {
+          create: followUp,
+        },
+      },
+    });
+  }
 }
 
 
