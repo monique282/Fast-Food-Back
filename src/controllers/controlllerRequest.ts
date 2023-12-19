516d77952d336c659e5ef365857e90994e36a743
monique <moniquejp16@gmail.com>
2023-12-19 03:26:27 -0300

    feat: separation of typing, creation of protocols

--- src/controllers/controlllerRequest.ts
+++ src/controllers/controlllerRequest.ts
@@ -1,33 +1,13 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import serviceRequest from "../services/serviceRequest";
+import { RequestSchemaTotalType } from '@/protocols';
 
 export async function postRequest(req: Request, res: Response) {
-    const choice = req.body as RequestSchemaTotalType;
+    const choice = req.body as RequestSchemaTotalType[];
 
     const result = await serviceRequest.postRequest(choice);
 
     return res.status(httpStatus.CREATED).send(result);
 }
 
-export type FollowUpType = {
-    id: number;
-    item: string;
-    price: string;
-};
-
-export type RequestSchemaType = {
-    id: number;
-    image: string;
-    name: string;
-    price: number;
-    description: string;
-};
-
-export type RequestSchemaTotalType = {
-    ProductSpecific: RequestSchemaType;
-    counter: number;
-    followUp?: FollowUpType;
-    observationText: string;
-    total: string;
-};
