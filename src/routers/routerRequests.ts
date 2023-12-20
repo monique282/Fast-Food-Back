3e0de37885332e4235b436d0db35ff13d62e30b1
monique <moniquejp16@gmail.com>
2023-12-20 15:23:22 -0300

    feat: taking orders routers

--- src/routers/routerRequests.ts
+++ src/routers/routerRequests.ts
@@ -1,4 +1,4 @@
-import { postRequest } from '@/controllers/controlllerRequest';
+import { getRequest, postRequest } from '@/controllers/controlllerRequest';
 import { validateJoiForAll } from '@/middlewares/middlewaresValidate';
 import { RequestSchemaTotal } from '@/schemas/schemasRequest';
 import { Router } from 'express';
@@ -6,6 +6,7 @@ import { Router } from 'express';
 const RequestRouter = Router();
 
 RequestRouter.post('/request', validateJoiForAll(RequestSchemaTotal), postRequest)
+RequestRouter.get('/request', getRequest)
 
 export { RequestRouter };
 
