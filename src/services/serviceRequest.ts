50758b74a0a1721c53748d2a80acfffa89f3adf1
monique <moniquejp16@gmail.com>
2023-12-20 15:17:32 -0300

    feat: taking orders

--- src/services/serviceRequest.ts
+++ src/services/serviceRequest.ts
@@ -2,14 +2,18 @@
 import { RequestSchemaTotalType } from "@/protocols";
 import { repositoryRequest } from "@/repositories/repositoryRequest";
 
-
 async function postRequest(choice: RequestSchemaTotalType[]) {
     const result = await repositoryRequest.postRequest(choice);
     return result;
 };
 
+async function getRequest() {
+    const allRequest = await repositoryRequest.getRequest();
+    return allRequest;
+};
+
 const serviceRequest = {
-    postRequest
+    postRequest, getRequest
 };
 
 export default serviceRequest;
