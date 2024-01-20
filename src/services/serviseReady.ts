fbdb4b5f90a99cb3d9017d45671c3c7b76c9c989
monique <moniquejp16@gmail.com>
2024-01-20 11:22:09 -0300

    feat: if the code exists

--- src/services/serviseReady.ts
+++ src/services/serviseReady.ts
@@ -4,6 +4,9 @@ import { repositoryRequest } from "@/repositories/repositoryRequest";
 
 async function postReady(code: number) {
   const readyTrueCodeExists = await repositoryRequest.getRequestCodeExist(code);
+  if (readyTrueCodeExists.length === 0) {
+    throw notFound("Código não encontrado");
+  };
   for (const item of readyTrueCodeExists) {
     const realy = item.ready;
     if (realy === true) {
